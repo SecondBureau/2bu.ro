28d15384a359b005c01516590ef285cbba46b92c
Gilles Crofils <gilles@secondbureau.com>
2010-11-01 11:31:08 +0100

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buro::Application.config.secret_token = '17dbfb4e9d8b5483fb5f14e3cdb34931ed44d2825aeeb368a397da317154b3db7d95a5711e4a9573c74c4623247be5f6b121d7463980bc3cdb88358f052f1951'
