10c51fa7b8da454520462fa61a3a636f82e58366
Gilles Crofils <gilles@secondbureau.com>
2011-01-11 21:53:13 +0800

    #362

--- lib/url_validator.rb
+++ lib/url_validator.rb
@@ -7,7 +7,7 @@ class UrlValidator < ActiveModel::EachValidator
   def validate_each(r, a, v)
     configuration = { :message => "is not valid or not responding", :with => nil }
     raise(ArgumentError, "A regular expression must be supplied as the :with option of the configuration hash") unless options[:with].is_a?(Regexp)
-        if v.to_s =~ configuration[:with] # check RegExp
+        if v.to_s =~ options[:with] # check RegExp
               begin # check header response
                   case Net::HTTP.get_response(URI.parse(v))
                     when Net::HTTPMovedPermanently
