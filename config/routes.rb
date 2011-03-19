9bbe0255b7a45872767ec8de4de1af5578ecb0d1
Gilles Crofils <gilles@secondbureau.com>
2011-03-19 16:37:30 +0800

    added squid

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Buro::Application.routes.draw do
   resources :redirections
   match '/newrelic_2buro' => 'redirections#newrelic'
   match '/whoami' => 'redirections#whoami'
+  match '/squid/:proxy' => 'redirections#squid'
   match '/:permalink' => 'redirections#redirect', :permalink => /[A-Za-z0-9_]{6,32}/
   match '/indirect/:permalink' => 'redirections#indirect', :permalink => /[A-Za-z0-9_]{6,32}/, :constraints => { :user_agent => /SecondBureau Redirector/ } 
   match '*path'  => "home#index"
