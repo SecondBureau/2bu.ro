62c23917f27a4e8d5363b5710c7eba61208f6202
Gilles Crofils <gilles@secondbureau.com>
2011-11-20 23:49:27 +0800

    updated routes to allow 4 chars only

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Buro::Application.routes.draw do
   resources :redirections
   match '/newrelic_2buro' => 'redirections#newrelic'
   match '/whoami' => 'redirections#whoami'
-  match '/:permalink' => 'redirections#redirect', :permalink => /[A-Za-z0-9_]{6,32}/
-  match '/indirect/:permalink' => 'redirections#indirect', :permalink => /[A-Za-z0-9_]{6,32}/, :constraints => { :user_agent => /SecondBureau Redirector/ } 
+  match '/:permalink' => 'redirections#redirect', :permalink => /[A-Za-z0-9_]{4,32}/
+  match '/indirect/:permalink' => 'redirections#indirect', :permalink => /[A-Za-z0-9_]{4,32}/, :constraints => { :user_agent => /SecondBureau Redirector/ } 
   match '*path'  => "home#index"
   root :to => "home#index"
 end
